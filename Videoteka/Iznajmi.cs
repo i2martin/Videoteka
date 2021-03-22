b5a5a9d28ec995db5f66208ae7d89d11167dbc53
IVan Martinović <tabloid66@gmail.com>
2021-03-22 09:29:45 +0100

    Zadnja

--- Videoteka/Iznajmi.cs
+++ Videoteka/Iznajmi.cs
@@ -2,6 +2,7 @@
 using System.Windows.Forms;
 using System.IO;
 using System.Reflection;
+using System.Collections;
 
 namespace Videoteka
 {
@@ -11,9 +12,10 @@ namespace Videoteka
         {
             InitializeComponent();
         }
-
+        public ArrayList Filmovi = new ArrayList();
         private void Iznajmi_Load(object sender, EventArgs e)
         {
+
             string line;
             string[] stringovi;
             using (StreamReader file = new StreamReader(Assembly.
@@ -30,6 +32,16 @@ namespace Videoteka
             {
                 ListaKorisnika.Items.Add(item);
             }
+
+            using (StreamReader file = new StreamReader(Assembly.
+                  GetExecutingAssembly().GetManifestResourceStream
+                  ("Videoteka.Resources.BazaFilmova.txt")))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    Filmovi.Add(line);
+                }
+            }
         }
 
         private void IznajmiButton_Click(object sender, EventArgs e)
@@ -37,7 +49,17 @@ namespace Videoteka
             //U datoteku Iznajmljivanja.txt dodati text u obliku korisnik +
             //iznajmljeni film.
             ListaFilmova.SelectedItem.ToString();
+            string str = ListaFilmova.SelectedItem.ToString() + "," + ListaKorisnika.SelectedItem.ToString();
+            using (StreamWriter stream = new StreamWriter(@"C:\Users\Korisnik\source\repos\Videoteka\Videoteka\Resources\Iznajmljivanja.txt"))
+            {
+                stream.WriteLine(str);
+                MessageBox.Show("Rezervacija filma uspješno dodana!");
+            }
+
+            //promijena količine filmova
+            string[] Podijeli = new string[] { };
             
         }
     }
 }
+
